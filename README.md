8322a6c5f034c5619eb04a7b1f807da97855ee93
levvw <abdelraahmanmostafa@gmail.com>
2022-04-07 22:25:58 +0200

    made all create and read queries

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # fifa-db-backend
+
 Back of Fifa football management system for database project
+
+Create in that order
+Country > League > Club > Manager > Player
--- index.js
+++ index.js
@@ -11,39 +11,93 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
-  if (error) throw error;
-  console.log("The solution is: ", results[0].solution);
-});
-
 app.listen(process.env.PORT || 3000, () => {
   console.log("server started on port 3000");
 });
 
+// Here are all the Creates
 app.post("/player", (req, res) => {
   connection.query(
-    `INSERT INTO player
-      VALUES ();`,
+    `INSERT INTO player(Pace, Dribbling, Physical, Passing, Shooting, Defending, Pname, Image, CID, CountryName)
+      VALUES (${req.body.pace},${req.body.dribbling},${req.body.physical},${req.body.passing},${req.body.shooting},${req.body.defending},'${req.body.pname}','${req.body.image}','${req.body.cid}','${req.body.countryname}');`,
     function (error, results, fields) {
       if (error) throw error;
-      console.log("The solution is: ", results[0].solution);
+      console.log("inserted");
     }
   );
 });
 app.post("/club", (req, res) => {
-  // create club
-});
-app.post("/league", (req, res) => {
-  // create league
-});
-app.get("players", (req, res) => {
-  // get players
-});
-app.get("clubs", (req, res) => {
-  // get clubs
-});
-app.get("leagues", (req, res) => {
-  // get leagues
+  connection.query(
+    `INSERT INTO club(ClubName, EstablishDate, Street, ZIP, City)
+      VALUES ('${req.body.club}','${req.body.date}','${req.body.street}','${req.body.zip}','${req.body.city}');`,
+    function (error, results, fields) {
+      if (error) throw error;
+      res.send("inserted");
+    }
+  );
 });
 
-connection.end();
+app.post("/country", (req, res) => {
+  connection.query(
+    `INSERT INTO country(CountryName, Continent)
+      VALUES ('${req.body.country}','${req.body.continent}');`,
+    function (error, results, fields) {
+      if (error) throw error;
+      res.send("inserted");
+    }
+  );
+});
+
+app.post("/league", (req, res) => {
+  connection.query(
+    `INSERT INTO league(LeagueName, Category, CountryName)
+      VALUES ('${req.body.league}','${req.body.category}','${req.body.country}');`,
+    function (error, results, fields) {
+      if (error) throw error;
+      res.send("insereted");
+    }
+  );
+});
+
+app.post("/manager", (req, res) => {
+  connection.query(
+    `INSERT INTO manager(ManagerName, CID)
+      VALUES ('${req.body.manager}','${req.body.cid}');`,
+    function (error, results, fields) {
+      if (error) throw error;
+      res.send("insereted");
+    }
+  );
+});
+
+// Here are all the gets
+app.get("/players", (req, res) => {
+  connection.query(`SELECT * from player`, function (error, results, fields) {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+app.get("/clubs", (req, res) => {
+  connection.query(`SELECT * from club`, function (error, results, fields) {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+app.get("/leagues", (req, res) => {
+  connection.query(`SELECT * from league`, function (error, results, fields) {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+app.get("/managers", (req, res) => {
+  connection.query(`SELECT * from manager`, function (error, results, fields) {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+app.get("/countries", (req, res) => {
+  connection.query(`SELECT * from country`, function (error, results, fields) {
+    if (error) throw error;
+    res.send(results);
+  });
+});
